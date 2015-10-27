8791652b38b3bd6eed6569df72a4d33899a5e0d1
Laetitia Massa <laetitiamassa@gmail.com>
2015-10-27 15:33:36 +0100

    added paying users for each bar in admin dasboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -55,34 +55,174 @@ ActiveAdmin.register_page "Dashboard" do
             panel "Offre de remplacement par barreau" do
                 ul do
                     #le bar en question + le nombre de users dont le barreau est celui-là
-                        li "Bruxelles OBFG : " do span User.where(:bar =>"BrusselsFR").count end
-                        li "Bruxelles OVB : " do span User.where(:bar =>"BrusselsNL").count end
-                        li "Arlon : " do span User.where(:bar =>"Arlon").count end
-                        li "Charleroi : " do span User.where(:bar =>"Charleroi").count end
-                        li "Dinant : " do span User.where(:bar =>"Dinant").count end
-                        li "Eupen : " do span User.where(:bar =>"Eupen").count end
-                        li "Huy : " do span User.where(:bar =>"Huy").count end
-                        li "Liège : " do span User.where(:bar =>"Liege").count end
-                        li "Marche : " do span User.where(:bar =>"Marche").count end
-                        li "Mons : " do span User.where(:bar =>"Mons").count end
-                        li "Namur : " do span User.where(:bar =>"Namur").count end
-                        li "Neufchateau : " do span User.where(:bar =>"Neufchateau").count end
-                        li "Nivelles : " do span User.where(:bar =>"Nivelles").count end
-                        li "Tournai : " do span User.where(:bar =>"Tournai").count end
-                        li "Verviers : " do span User.where(:bar =>"Anvers").count end
-                        li "Anvers : " do span User.where(:bar =>"Anvers").count end
-                        li "Brugge : " do span User.where(:bar =>"Brugge").count end
-                        li "Dendermonde : " do span User.where(:bar =>"Dendermonde").count end
-                        li "Gent : " do span User.where(:bar =>"Gent").count end
-                        li "Hasselt : " do span User.where(:bar =>"Hasselt").count end
-                        li "Ieper : " do span User.where(:bar =>"Ieper").count end
-                        li "Kortrijk : " do span User.where(:bar =>"Kortrijk").count end
-                        li "Leuven : " do span User.where(:bar =>"Leuven").count end
-                        li "Mechelen : " do span User.where(:bar =>"Mechelen").count end
-                        li "Oudernaarde : " do span User.where(:bar =>"Oudernaarde").count end
-                        li "Tongeren : " do span User.where(:bar =>"Tongeren").count end
-                        li "Turnhout : " do span User.where(:bar =>"Turnhout").count end
-                        li "Veurne : " do span User.where(:bar =>"Veurne").count end
+                        li "Bruxelles OBFG : " do 
+                            span User.where(:bar =>"BrusselsFR").count
+                            span "/"
+                            span User.where(:bar =>"BrusselsFR", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Bruxelles OVB : " do 
+                            span User.where(:bar =>"BrusselsNL").count 
+                            span "/"
+                            span User.where(:bar =>"BrusselsNL", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Arlon : " do 
+                            span User.where(:bar =>"Arlon").count 
+                            span "/"
+                            span User.where(:bar =>"Arlon", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Charleroi : " do 
+                            span User.where(:bar =>"Charleroi").count
+                            span "/"
+                            span User.where(:bar =>"Charleroi", :subscribed => true).count
+                            span "payants"
+                         end
+                        li "Dinant : " do 
+                            span User.where(:bar =>"Dinant").count
+                            span "/"
+                            span User.where(:bar =>"Dinant", :subscribed => true).count
+                            span "payants" 
+                        end
+                        li "Eupen : " do 
+                            span User.where(:bar =>"Eupen").count 
+                            span "/"
+                            span User.where(:bar =>"Eupen", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Huy : " do 
+                            span User.where(:bar =>"Huy").count
+                            span "/"
+                            span User.where(:bar =>"Huy", :subscribed => true).count
+                            span "payants" 
+                        end
+                        li "Liège : " do 
+                            span User.where(:bar =>"Liege").count 
+                            span "/"
+                            span User.where(:bar =>"Liege", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Marche : " do 
+                            span User.where(:bar =>"Marche").count 
+                            span "/"
+                            span User.where(:bar =>"Marche", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Mons : " do 
+                            span User.where(:bar =>"Mons").count 
+                            span "/"
+                            span User.where(:bar =>"Mons", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Namur : " do 
+                            span User.where(:bar =>"Namur").count 
+                            span "/"
+                            span User.where(:bar =>"Namur", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Neufchateau : " do 
+                            span User.where(:bar =>"Neufchateau").count 
+                            span "/"
+                            span User.where(:bar =>"Neufchateau", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Nivelles : " do 
+                            span User.where(:bar =>"Nivelles").count 
+                            span "/"
+                            span User.where(:bar =>"Nivelles", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Tournai : " do 
+                            span User.where(:bar =>"Tournai").count 
+                            span "/"
+                            span User.where(:bar =>"Tournai", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Verviers : " do 
+                            span User.where(:bar =>"Anvers").count 
+                            span "/"
+                            span User.where(:bar =>"Anvers", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Anvers : " do 
+                            span User.where(:bar =>"Anvers").count 
+                            span "/"
+                            span User.where(:bar =>"Anvers", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Brugge : " do 
+                            span User.where(:bar =>"Brugge").count 
+                            span "/"
+                            span User.where(:bar =>"Brugge", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Dendermonde : " do 
+                            span User.where(:bar =>"Dendermonde").count 
+                            span "/"
+                            span User.where(:bar =>"Dendermonde", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Gent : " do 
+                            span User.where(:bar =>"Gent").count 
+                            span "/"
+                            span User.where(:bar =>"Gent", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Hasselt : " do 
+                            span User.where(:bar =>"Hasselt").count 
+                            span "/"
+                            span User.where(:bar =>"Hasselt", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Ieper : " do 
+                            span User.where(:bar =>"Ieper").count 
+                            span "/"
+                            span User.where(:bar =>"Ieper", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Kortrijk : " do 
+                            span User.where(:bar =>"Kortrijk").count 
+                            span "/"
+                            span User.where(:bar =>"Kortrijk", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Leuven : " do 
+                            span User.where(:bar =>"Leuven").count 
+                            span "/"
+                            span User.where(:bar =>"Leuven", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Mechelen : " do 
+                            span User.where(:bar =>"Mechelen").count 
+                            span "/"
+                            span User.where(:bar =>"Mechelen", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Oudernaarde : " do 
+                            span User.where(:bar =>"Oudernaarde").count 
+                            span "/"
+                            span User.where(:bar =>"Oudernaarde", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Tongeren : " do 
+                            span User.where(:bar =>"Tongeren").count 
+                            span "/"
+                            span User.where(:bar =>"Tongeren", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Turnhout : " do 
+                            span User.where(:bar =>"Turnhout").count 
+                            span "/"
+                            span User.where(:bar =>"Turnhout", :subscribed => true).count
+                            span "payants"
+                        end
+                        li "Veurne : " do 
+                            span User.where(:bar =>"Veurne").count 
+                            span "/"
+                            span User.where(:bar =>"Veurne", :subscribed => true).count
+                            span "payants"
+                        end
 
                 end
             end
